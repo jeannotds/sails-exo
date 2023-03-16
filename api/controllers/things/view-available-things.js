5b6eda0d2bc77528c37e5c9efa705a53d49376f2
jeannot diambu <diambujeannot@gmail.com>
2023-03-16 13:56:48 +0100

    Feat : add advanted request and .populate

--- api/controllers/things/view-available-things.js
+++ api/controllers/things/view-available-things.js
@@ -11,8 +11,16 @@ module.exports = {
   },
 
   fn: async function (inputs, exits) {
+    // Send my data and all of my friends
+    var me = await User.findOne({
+      id: this.req.me.id,
+    }).populate("friends");
+
+    //Send All my fiends Id
+    var friendIds = _.pluck(me.friends, "id");
+
     var things = await Thing.find({
-      owner: this.req.me.id,
+      or: [{ owner: this.req.me.id }, { owner: { in: friendIds } }],
     });
     return exits.success({ things });
   },
